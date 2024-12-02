325e7f24077d4225f19bbb408bbcf37f9d19db11
Sean Reilly <seanjreilly@gmail.com>
2024-12-02 07:02:11 +0000

    Day 2, part 2
    For cases where the levels aren't immediately valid,
    loop through the levels one at a time and construct
    a list of levels without that input and try again.
    If any of those work, return true.
    
    This is O(n^2), but still performant enough, and
    there are some tricky instances in my input that
    would make solving the problem in a "cleverer" way
    a bit tricky, such as when the two first numbers in
    the sequence match.

--- src/main/kotlin/day02/Day02Solution.kt
+++ src/main/kotlin/day02/Day02Solution.kt
@@ -6,7 +6,7 @@ fun main() = Day02Solution().run()
 class Day02Solution : IntSolution() {
     override fun part1(input: List<String>) = input.map { parseLine(it) }.count { isSafe(it) }
 
-    override fun part2(input: List<String>) = 0
+    override fun part2(input: List<String>) = input.map { parseLine(it) }.count { isSafe2(it) }
 }
 
 internal fun parseLine(line: String) = line.split(' ').map(String::toInt)
@@ -15,5 +15,15 @@ internal fun isSafe(levels: List<Int>): Boolean {
     return when (levels[1] > levels[0]) {
         true -> levels.windowed(2).all { (first, second) -> second - first in allowedRange }
         false -> levels.windowed(2).all { (first, second) -> first - second in allowedRange }
+    }
+}
+internal fun isSafe2(levels: List<Int>): Boolean {
+    if (isSafe(levels)) {
+        return true
+    }
+    val indexedLevels = levels.withIndex()
+    return (0 until levels.size).any { i ->
+        val dampenedLevels = indexedLevels.filter { it.index != i  }.map { it.value }
+        isSafe(dampenedLevels)
     }
 }
--- src/test/kotlin/day02/Day02SolutionTest.kt
+++ src/test/kotlin/day02/Day02SolutionTest.kt
@@ -58,8 +58,32 @@ class Day02SolutionTest {
     }
 
     @Test
+    fun `isSafe2 should return true if removing a single level from the input would make it safe`() {
+        assert(isSafe2(parseLine(sampleInput[0])))
+        assert(isSafe2(parseLine(sampleInput[3])))
+        assert(isSafe2(parseLine(sampleInput[4])))
+        assert(isSafe2(parseLine(sampleInput[5])))
+    }
+
+    @Test
+    fun `isSafe2 should return false if removing any single level from the input still won't make it safe`() {
+        assert(!isSafe2(parseLine(sampleInput[1])))
+        assert(!isSafe2(parseLine(sampleInput[2])))
+    }
+
+    @Test
+    fun `isSafe2 should return true for otherwise valid levels where the first two numbers match`() {
+        assert(isSafe2(listOf(75, 75, 76, 77, 79, 81)))
+    }
+
+    @Test
     fun `part1 should count the number of safe levels`() {
         assert(solution.part1(sampleInput) == 2)
+    }
+
+    @Test
+    fun `part2 should count the number of safe levels with dampening`() {
+        assert(solution.part2(sampleInput) == 4)
     }
 }
 
