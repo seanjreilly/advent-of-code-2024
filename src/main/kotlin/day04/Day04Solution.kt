c1fde8d2a12022d3025373cf563ec814db08d185
Sean Reilly <seanjreilly@gmail.com>
2024-12-03 10:48:11 +0000

    Day 4 skeleton

---
+++ src/main/kotlin/day04/Day04Solution.kt
@@ -0,0 +1,12 @@
+package day04
+
+import utils.LongSolution
+
+fun main() = Day04Solution().run()
+class Day04Solution : LongSolution() {
+    override fun part1(input: List<String>): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(input: List<String>) = 0L
+}
---
+++ src/test/kotlin/day04/Day04SolutionTest.kt
@@ -0,0 +1,9 @@
+package day04
+
+class Day04SolutionTest {
+    private val sampleInput = """
+    """.trimIndent().lines()
+
+    private val solution = Day04Solution()
+
+}
