fb168877124baf72a73476e211f6c4fcd823c729
Sean Reilly <seanjreilly@gmail.com>
2024-12-03 06:52:05 +0000

    Day 3, part 2
    Split on occurrences of "do()", remove everything
    in each match after a "don't()", and then join the
    results into a single string and run the part1
    functionality on it.

--- src/main/kotlin/day03/Day03Solution.kt
+++ src/main/kotlin/day03/Day03Solution.kt
@@ -5,18 +5,31 @@ import utils.IntSolution
 fun main() = Day03Solution().run()
 class Day03Solution : IntSolution() {
     override fun part1(input: List<String>): Int {
-        return findMulInstructions(input).sumOf { (x, y) -> x * y }
+        return findMulInstructions(input.joinToString("")).sumOf { (x, y) -> x * y }
     }
 
-    override fun part2(input: List<String>) = 0
+    override fun part2(input: List<String>) : Int {
+        return findMulInstructions(findEnabledMulInstructions(input.joinToString(""))).sumOf { (x, y) -> x * y }
+    }
 }
 
 private val PARSE_INSTRUCTION_REGEX = """mul\((\d+),(\d+)\)""".toRegex()
 
-internal fun findMulInstructions(input: List<String>): Sequence<Pair<Int, Int>> = sequence {
-    val line = input.joinToString("")
+internal fun findMulInstructions(line: String): Sequence<Pair<Int, Int>> = sequence {
     PARSE_INSTRUCTION_REGEX.findAll(line).forEach { match ->
         val groups = match.groups
         yield(groups[1]!!.value.toInt() to groups[2]!!.value.toInt())
     }
+}
+
+val DO_INSTRUCTION_REGEX = """do\(\)""".toRegex()
+internal fun findEnabledMulInstructions(input: String): String {
+    return DO_INSTRUCTION_REGEX.splitToSequence(input)
+        .joinToString("") {
+            val index = it.indexOf("don't()")
+            when (index) {
+                -1 -> it
+                else -> it.substring(0 until index)
+            }
+        }
 }
--- src/test/kotlin/day03/Day03SolutionTest.kt
+++ src/test/kotlin/day03/Day03SolutionTest.kt
@@ -4,9 +4,22 @@ import org.junit.jupiter.api.Test
 
 class Day03SolutionTest {
     private val sampleInput = """
+        xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+    """.trim()
+
+    private val sampleInputOnMultipleLines = """
         xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)
         +mul(32,64]then(mul(11,8)mul(8,5))
-    """.trim().lines()
+    """.trimIndent().lines()
+
+    private val sampleInputPart2 = """
+        xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+    """.trim()
+
+    private val sampleInputPart2OnMultipleLines = """
+        xmul(2,4)&mul[3,7]!^don't()_mul(5,5)
+        +mul(32,64](mul(11,8)undo()?mul(8,5))
+    """.trimIndent().lines()
 
     private val solution = Day03Solution()
 
@@ -24,7 +37,42 @@ class Day03SolutionTest {
     }
 
     @Test
+    fun `findEnabledMulInstructions should find instructions before the initial don't() instruction`() {
+        val expectedResult = "mul(1,2)mul(2,3)asdf"
+        val input = "${expectedResult}don't()mul(3,4)ailhr"
+
+        val result = findEnabledMulInstructions(input)
+
+        assert(result == expectedResult)
+    }
+
+    @Test
+    fun `findEnabledMulInstructions should remove instructions after a don't() instruction but before a do instruction`() {
+        val input = "abcddon't()efghijdo()klmnopqdon't()rstudo()vwxyz"
+        val expectedResult = "abcdklmnopqvwxyz"
+
+        val result = findEnabledMulInstructions(input)
+
+        assert(result == expectedResult)
+    }
+
+    @Test
     fun `part1 should return the sum of all valid mul instructions in the input`() {
-        assert(solution.part1(sampleInput) == 161)
+        assert(solution.part1(listOf(sampleInput)) == 161)
+    }
+
+    @Test
+    fun `part1 should work with input split over multiple lines`() {
+        assert(solution.part1(sampleInputOnMultipleLines) == 161)
+    }
+
+    @Test
+    fun `part2 should return the sum of all valid and enabled mul instructions in the input`() {
+        assert(solution.part2(listOf(sampleInputPart2)) == 48)
+    }
+
+    @Test
+    fun `part2 should work with input split over multiple lines`() {
+        assert(solution.part2(sampleInputPart2OnMultipleLines) == 48)
     }
 }
