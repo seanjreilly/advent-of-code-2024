e9a3c674c05885db7e70428889ec4a210e004c2e
Sean Reilly <seanjreilly@gmail.com>
2024-12-05 21:25:56 +0000

    Day 6 skeleton

---
+++ src/main/kotlin/day06/Day06Solution.kt
@@ -0,0 +1,12 @@
+package day06
+
+import utils.IntSolution
+
+fun main() = Day06Solution().run()
+class Day06Solution : IntSolution() {
+    override fun part1(input: List<String>): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(input: List<String>) = 0
+}
---
+++ src/test/kotlin/day06/Day06SolutionTest.kt
@@ -0,0 +1,8 @@
+package day06
+
+class Day06SolutionTest {
+    private val sampleInput = """
+        """.trimIndent().lines()
+
+    private val solution = Day06Solution()
+}
