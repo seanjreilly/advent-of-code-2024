f7e727a1ec6663e02ec33775cbcf27b9efdbe18c
Sean Reilly <seanjreilly@gmail.com>
2024-12-06 05:42:20 +0000

    Day 6, part 1
    Build the "move or turn right" logic using points
    and directions, then move until the guard leaves the
    bounds of the map, adding the current point to a set
    after every move. Return the size of the set.

--- src/main/kotlin/day06/Day06Solution.kt
+++ src/main/kotlin/day06/Day06Solution.kt
@@ -1,12 +1,41 @@
 package day06
 
+import utils.Bounds
+import utils.CardinalDirection.North
 import utils.IntSolution
+import utils.Point
+import utils.PointAndDirection
+import utils.TurnDirection
+import utils.get
 
 fun main() = Day06Solution().run()
 class Day06Solution : IntSolution() {
     override fun part1(input: List<String>): Int {
-        TODO("Not yet implemented")
+        val bounds = Bounds(input)
+        var guard = findStart(input)
+        val pointsVisited = mutableSetOf<Point>()
+        do {
+            pointsVisited += guard.point
+            guard = moveNext(guard, input)
+        } while (guard.point in bounds)
+        return pointsVisited.size
     }
 
     override fun part2(input: List<String>) = 0
+}
+
+internal fun findStart(input: List<String>): PointAndDirection {
+    val bounds = Bounds(input)
+    return PointAndDirection(bounds.first { input[it] == '^' }, North)
+}
+
+internal fun moveNext(pointAndDirection: PointAndDirection, grid: List<String>): PointAndDirection {
+    val (point, direction) = pointAndDirection
+    val bounds = Bounds(grid)
+
+    val newPoint = point.move(direction)
+    if (newPoint !in bounds || grid[newPoint] != '#') {
+        return PointAndDirection(newPoint, direction)
+    }
+    return PointAndDirection(point, direction.turn(TurnDirection.Right))
 }
--- src/test/kotlin/day06/Day06SolutionTest.kt
+++ src/test/kotlin/day06/Day06SolutionTest.kt
@@ -1,8 +1,140 @@
 package day06
 
+import org.junit.jupiter.api.Test
+import utils.CardinalDirection.*
+import utils.Point
+import utils.PointAndDirection
+
 class Day06SolutionTest {
     private val sampleInput = """
+        ....#.....
+        .........#
+        ..........
+        ..#.......
+        .......#..
+        ..........
+        .#..^.....
+        ........#.
+        #.........
+        ......#...
         """.trimIndent().lines()
 
     private val solution = Day06Solution()
+    
+    @Test
+    fun `findStart should return the location of the ^ symbol, pointing North`() {
+        val expectedResult = PointAndDirection(Point(4, 6), North)
+
+        val result: PointAndDirection = findStart(sampleInput)
+
+        assert(result == expectedResult)
+    }
+
+    @Test
+    fun `moveNext should move forward one point if there is no obstacle in that direction, when facing North`() {
+        val originalDirection = North
+        val start = PointAndDirection(Point(4, 6), originalDirection)
+
+        val result : PointAndDirection = moveNext(start, sampleInput)
+
+        assert(result.point == Point(4, 5))
+        assert(result.direction == originalDirection)
+    }
+
+    @Test
+    fun `moveNext should move forward one point if there is no obstacle in that direction, when facing East`() {
+        val originalDirection = East
+        val start = PointAndDirection(Point(4, 6), originalDirection)
+
+        val result : PointAndDirection = moveNext(start, sampleInput)
+
+        assert(result.point == Point(5, 6))
+        assert(result.direction == originalDirection)
+    }
+
+    @Test
+    fun `moveNext should move forward one point if there is no obstacle in that direction, when facing South`() {
+        val originalDirection = South
+        val start = PointAndDirection(Point(4, 6), originalDirection)
+
+        val result : PointAndDirection = moveNext(start, sampleInput)
+
+        assert(result.point == Point(4, 7))
+        assert(result.direction == originalDirection)
+    }
+
+    @Test
+    fun `moveNext should move forward one point if there is no obstacle in that direction, when facing West`() {
+        val originalDirection = West
+        val start = PointAndDirection(Point(4, 6), originalDirection)
+
+        val result : PointAndDirection = moveNext(start, sampleInput)
+
+        assert(result.point == Point(3, 6))
+        assert(result.direction == originalDirection)
+    }
+
+    @Test
+    fun `moveNext should turn right if there is an obstacle in the current direction`() {
+        val originalPosition = Point(4, 1)
+        val start = PointAndDirection(originalPosition, North)
+
+        val result : PointAndDirection = moveNext(start, sampleInput)
+
+        assert(result.point == originalPosition)
+        assert(result.direction == East)
+    }
+
+    @Test
+    fun `moveNext should turn right until there is a free path`() {
+        val grid = """
+            .#.
+            .^#
+            .#.
+        """.trimIndent().lines()
+
+        val start = findStart(grid)
+        assert(start.direction == North)
+
+        //should turn right
+        var currentPointAndDirection = moveNext(start, grid)
+        assert(currentPointAndDirection.point == start.point)
+        assert(currentPointAndDirection.direction == East)
+
+        //should turn right again
+        currentPointAndDirection = moveNext(currentPointAndDirection, grid)
+        assert(currentPointAndDirection.point == start.point)
+        assert(currentPointAndDirection.direction == South)
+
+        //should turn right again
+        currentPointAndDirection = moveNext(currentPointAndDirection, grid)
+        assert(currentPointAndDirection.point == start.point)
+        assert(currentPointAndDirection.direction == West)
+
+        //should finally move forward
+        currentPointAndDirection = moveNext(currentPointAndDirection, grid)
+        assert(currentPointAndDirection.point == Point(0,1))
+        assert(currentPointAndDirection.direction == West)
+    }
+
+    @Test
+    fun `moveNext should not throw an exception when moving out of the grid`() {
+        val grid = """
+            .^.
+            ...
+            ...
+        """.trimIndent().lines()
+
+        val start = findStart(grid)
+        assert(start.point == Point(1, 0)) { "precondition check on starting point" }
+
+        val exit = moveNext(start, grid)
+        assert(exit.point == Point(1, -1))
+        assert(exit.direction == North)
+    }
+
+    @Test
+    fun `part1 should move until the guard leaves the map and return the number of unique points visited, ignoring direction`() {
+        assert(solution.part1(sampleInput) == 41)
+    }
 }
