51715c09dd90c84d95bd3b66ac5dcaba9542d110
Sean Reilly <seanjreilly@gmail.com>
2024-12-06 06:18:22 +0000

    Optimise Day 6, part 2
    Second optimisation: only consider the points the
    guard actually visits when leaving the grid.
    
    Solution improves from ~1000 ms to just under
    500 ms.

--- src/main/kotlin/day06/Day06Solution.kt
+++ src/main/kotlin/day06/Day06Solution.kt
@@ -11,21 +11,14 @@ import utils.get
 fun main() = Day06Solution().run()
 class Day06Solution : IntSolution() {
     override fun part1(input: List<String>): Int {
-        val bounds = Bounds(input)
-        var guard = findStart(input)
-        val pointsVisited = mutableSetOf<Point>()
-        do {
-            pointsVisited += guard.point
-            guard = moveNext(guard, input)
-        } while (guard.point in bounds)
-        return pointsVisited.size
+        return findPointsVisitedBeforeExiting(input).size
     }
 
     override fun part2(input: List<String>) : Int {
-        val bounds = Bounds(input)
-        return bounds
-            .filter { input[it] == '.' }
+        //adding an obstruction to a point the guard doesn't visit cannot create a loop
+        return findPointsVisitedBeforeExiting(input)
             .parallelStream()
+            .filter { input[it] == '.' }
             .map { potentialObstruction -> addObstruction(input, potentialObstruction) }
             .filter { gridWithNewObstruction -> detectLoop(gridWithNewObstruction) }
             .count()
@@ -45,6 +38,17 @@ internal fun detectLoop(grid: List<String>): Boolean {
         previousPositions += guard
     } while (guard.point in bounds)
     return false
+}
+
+private fun findPointsVisitedBeforeExiting(input: List<String>): MutableSet<Point> {
+    val bounds = Bounds(input)
+    var guard = findStart(input)
+    val pointsVisited = mutableSetOf<Point>()
+    do {
+        pointsVisited += guard.point
+        guard = moveNext(guard, input)
+    } while (guard.point in bounds)
+    return pointsVisited
 }
 
 internal fun moveNext(pointAndDirection: PointAndDirection, grid: List<String>): PointAndDirection {
