1a310f22046289607c130d0f39a9d923da969643
Sean Reilly <seanjreilly@gmail.com>
2024-12-07 06:50:09 +0000

    Day 7, part 1
    Brute force approach: run through the
    exponential (yikes) combination of operators
    on all the inputs and see if any of them can
    produce the testValue.
    
    Return the sum of the testValues of all
    equations that can yield the right answer.
    
    Part 1 runs in ~50ms.

--- src/main/kotlin/day07/Day07Solution.kt
+++ src/main/kotlin/day07/Day07Solution.kt
@@ -5,8 +5,40 @@ import utils.LongSolution
 fun main() = Day07Solution().run()
 class Day07Solution : LongSolution() {
     override fun part1(input: List<String>): Long {
-        TODO("Not yet implemented")
+        return parseInput(input)
+            .filter { it.couldBeValid() }
+            .sumOf { it.testValue }
     }
 
     override fun part2(input: List<String>) = 0L
 }
+
+private val OPERATORS : List<(Long, Long) -> Long> = listOf(Long::plus, Long::times)
+internal data class CalibrationEquation(val testValue: Long, val inputs: List<Long>) {
+    fun couldBeValid(): Boolean {
+        var lastRoundOfResults = listOf(inputs.first())
+        var tail = inputs.drop(1)
+        while (tail.isNotEmpty()) {
+            val currentValue = tail.first()
+            tail = tail.drop(1)
+            val nextRoundOfResults = OPERATORS.flatMap { operator ->
+                val resultsToAdd: List<Long> = lastRoundOfResults.map { it: Long -> operator.invoke(it, currentValue) }
+                resultsToAdd
+            }
+            lastRoundOfResults = nextRoundOfResults
+        }
+        return testValue in lastRoundOfResults
+    }
+    constructor(testValue: Int, vararg inputs: Int ) : this(testValue.toLong(), inputs.map { it.toLong() })
+
+    companion object {
+        operator fun invoke(line: String) : CalibrationEquation {
+            val (rawTestValue, rawInputs) = line.split(": ", limit = 2)
+            val inputs = rawInputs.split(' ').filter { it.isNotBlank() }.map(String::toLong)
+            return CalibrationEquation(rawTestValue.toLong(), inputs)
+        }
+    }
+
+}
+
+internal fun parseInput(input: List<String>) = input.map { CalibrationEquation(it) }
--- src/test/kotlin/day07/Day07SolutionTest.kt
+++ src/test/kotlin/day07/Day07SolutionTest.kt
@@ -1,9 +1,82 @@
 package day07
 
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
 class Day07SolutionTest {
     private val sampleInput = """
+        190: 10 19
+        3267: 81 40 27
+        83: 17 5
+        156: 15 6
+        7290: 6 8 6 15
+        161011: 16 10 13
+        192: 17 8 14
+        21037: 9 7 18 13
+        292: 11 6 16 20
     """.trimIndent().lines()
 
     private val solution = Day07Solution()
 
+    @Test
+    fun `parseInput should return a list of calibration Equations`() {
+        val expectedResult = listOf(
+            CalibrationEquation(190, 10, 19),
+            CalibrationEquation(3267, 81, 40, 27),
+            CalibrationEquation(83, 17, 5),
+            CalibrationEquation(156, 15, 6),
+            CalibrationEquation(7290, 6, 8, 6, 15),
+            CalibrationEquation(161011, 16, 10, 13),
+            CalibrationEquation(192, 17, 8, 14),
+            CalibrationEquation(21037, 9, 7, 18, 13),
+            CalibrationEquation(292, 11, 6, 16, 20)
+        )
+
+        val result : List<CalibrationEquation> = parseInput(sampleInput)
+
+        assert (result.size == expectedResult.size)
+        result.forEachIndexed { index, result -> assert(result == expectedResult[index]) }
+    }
+
+    @Nested
+    inner class CalibrationEquationTest {
+        @Test
+        fun `constructor should return a Calibration Equation given a line`() {
+            val line = sampleInput.first()
+            val expectedResult = CalibrationEquation(190, 10, 19)
+
+            val equation = CalibrationEquation(line)
+
+            assert(equation == expectedResult)
+        }
+
+        @Test
+        fun `couldBeValid should return true when a combination of multiplying and adding the input terms from left to right can compute the testValue`() {
+            val viableEquations = listOf(
+                CalibrationEquation(190, 10, 19),
+                CalibrationEquation(3267, 81, 40, 27),
+                CalibrationEquation(292, 11, 6, 16, 20)
+            )
+
+            viableEquations.forEach { equation ->
+                val result : Boolean = equation.couldBeValid()
+                assert(result)
+            }
+        }
+
+        @Test
+        fun `couldBeValid should return false when no combination of multiplying and adding the input terms from left to right can compute the testValue`() {
+            val nonViableEquations = parseInput(sampleInput).drop(2).dropLast(1)
+
+            nonViableEquations.forEach { equation ->
+                val result : Boolean = equation.couldBeValid()
+                assert(!result)
+            }
+        }
+    }
+
+    @Test
+    fun `part1 should return the sum of the test values for all CalibrationEquations that could be valid`() {
+        assert(solution.part1(sampleInput) == 3749L)
+    }
 }
