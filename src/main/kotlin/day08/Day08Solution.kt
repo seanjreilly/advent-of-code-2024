9fa0d3a005046411534d6ef9d47871dacc230060
Sean Reilly <seanjreilly@gmail.com>
2024-12-08 07:01:20 +0000

    Day 8, part 1
    Parse the set of antenna locations for each
    frequency. Generate the two-element
    permutations for each set, and then find the
    points in line with both that are within the
    map boundary.
    
    Count the unique ones across all frequencies.

--- src/main/kotlin/day08/Day08Solution.kt
+++ src/main/kotlin/day08/Day08Solution.kt
@@ -1,12 +1,65 @@
 package day08
 
+import utils.Bounds
 import utils.LongSolution
+import utils.Point
+import utils.get
 
 fun main() = Day08Solution().run()
 class Day08Solution : LongSolution() {
     override fun part1(input: List<String>): Long {
-        TODO("Not yet implemented")
+        val bounds = Bounds(input)
+        return parseAntennaLocations(input).values
+            .flatMap { antennaLocations -> findAntinodes(antennaLocations, bounds) }
+            .toSet().size.toLong()
     }
 
     override fun part2(input: List<String>) = 0L
+}
+
+internal fun findAntinodes(points: Set<Point>, bounds: Bounds): Set<Point> {
+    return points
+        .generateTwoElementPairs()
+        .flatMap { (a,b) -> findAntinodesBetween(a, b, bounds) }
+        .toSet()
+}
+
+internal fun findAntinodesBetween(a: Point, b: Point, bounds: Bounds): Set<Point> {
+    val xDifference = a.x - b.x
+    val yDifference = a.y - b.y
+
+    val results = listOf(
+        Point(a.x + xDifference, a.y + yDifference),
+        Point(a.x - xDifference, a.y - yDifference),
+        Point(b.x + xDifference, b.y + yDifference),
+        Point(b.x - xDifference, b.y - yDifference)
+    )
+
+    return results
+        .filter { it != a }
+        .filter { it != b }
+        .filter { it in bounds }
+        .toSet()
+}
+
+internal fun <T> Set<T>.generateTwoElementPairs(): Sequence<Pair<T, T>> {
+    var values = this.toList()
+    return sequence {
+        while (values.isNotEmpty()) {
+            val first = values.first()
+            values = values.drop(1)
+            yieldAll(values.map { first to it })
+        }
+    }
+}
+
+internal typealias AntennaMap = Map<Char, Set<Point>>
+
+internal fun parseAntennaLocations(input: List<String>): AntennaMap {
+    val bounds = Bounds(input)
+    return bounds
+        .map { point -> point to input[point] }
+        .filter { (_, char) -> char.isLetterOrDigit() }
+        .groupBy({ (_, char) -> char }, { (point, _) -> point })
+        .mapValues { entry -> entry.value.toSet() }
 }
--- src/test/kotlin/day08/Day08SolutionTest.kt
+++ src/test/kotlin/day08/Day08SolutionTest.kt
@@ -1,8 +1,213 @@
 package day08
 
+import org.junit.jupiter.api.Test
+import utils.Bounds
+import utils.Point
+import utils.get
+
 class Day08SolutionTest {
     private val sampleInput = """
+        ............
+        ........0...
+        .....0......
+        .......0....
+        ....0.......
+        ......A.....
+        ............
+        ............
+        ........A...
+        .........A..
+        ............
+        ............
     """.trimIndent().lines()
 
     private val solution = Day08Solution()
+
+    @Test
+    fun `generateTwoElementPairs should generate all of the unique 2 element pairs of a Set in any order`() {
+        val set = setOf("A", "B", "C", "D")
+        val expectedResult = setOf(
+            "A" to "B",
+            "A" to "C",
+            "A" to "D",
+            "B" to "C",
+            "B" to "D",
+            "C" to "D"
+        )
+
+        val result : Sequence<Pair<String, String>> = set.generateTwoElementPairs()
+
+        assert(result.toSet() == expectedResult) //convert the result to a set to ignore order
+    }
+
+    @Test
+    fun `generateTwoElementPairs should work for Points`() {
+        val p1 = Point(1, 1)
+        val p2 = Point(2, 2)
+        val p3 = Point(3, 3)
+        val points = setOf(p1, p2, p3)
+
+        val expectedResult = setOf(
+            p1 to p2,
+            p1 to p3,
+            p2 to p3
+        )
+
+        val result : Sequence<Pair<Point, Point>> = points.generateTwoElementPairs()
+
+        assert(result.toSet() == expectedResult)
+    }
+
+    @Test
+    fun `parseAntennaLocations should return a map of antenna frequencies to antenna locations`() {
+        val antennaLocations : AntennaMap = parseAntennaLocations(sampleInput)
+        val expectedZeroLocations = setOf(
+            Point(8,1),
+            Point(5,2),
+            Point(7,3),
+            Point(4,4)
+        )
+
+        val expectedALocations = setOf(
+            Point(6,5),
+            Point(8,8),
+            Point(9,9)
+        )
+
+
+        assert(antennaLocations.keys == setOf('A', '0'))
+        assert(antennaLocations['0'] == expectedZeroLocations)
+        assert(antennaLocations['A'] == expectedALocations)
+    }
+
+    @Test
+    fun `findAntinodesBetween should return the points that are in line with both antennae and twice as far away from one as the other given antennae on a horizontal line`() {
+        val input = """
+            ..........
+            ...A..A...
+            ..........
+        """.trimIndent().lines()
+
+        val antennaMap = parseAntennaLocations(input)
+        val aLocations = antennaMap['A']!!
+        assert(aLocations == setOf(Point(3,1), Point(6,1))) { "precondition" }
+        val aLocationList = aLocations.toList()
+
+        val expectedResult = setOf(Point(0,1), Point(9,1))
+
+        val result : Set<Point> = findAntinodesBetween(aLocationList[0], aLocationList[1], Bounds(input))
+
+        assert(result == expectedResult)
+    }
+
+    @Test
+    fun `findAntinodesBetween should return the points that are in line with both antennae and twice as far away from one as the other given antennae on a vertical line`() {
+        val input = """
+            ...
+            ...
+            ...
+            .A.
+            ...
+            ...
+            .A.
+            ...
+            ...
+            ...
+        """.trimIndent().lines()
+
+        val antennaMap = parseAntennaLocations(input)
+        val aLocations = antennaMap['A']!!
+        assert(aLocations == setOf(Point(1,3), Point(1,6))) { "precondition" }
+        val aLocationList = aLocations.toList()
+
+        val expectedResult = setOf(Point(1, 0), Point(1, 9))
+
+        val result : Set<Point> = findAntinodesBetween(aLocationList[0], aLocationList[1], Bounds(input))
+
+        assert(result == expectedResult)
+    }
+    
+    @Test
+    fun `findAntinodesBetween should only return antinodes within the bounds of the grid`() {
+        val input = """
+            ..........
+            ..........
+            #.........
+            ....a.....
+            ........a.
+            ..........
+            ..........
+            ..........
+            ..........
+            ..........
+        """.trimIndent().lines()
+
+        val antennaMap = parseAntennaLocations(input)
+        val aLocations = antennaMap['a']!!
+        assert(aLocations == setOf(Point(4,3), Point(8,4))) { "precondition" }
+        val aLocationList = aLocations.toList()
+
+        val expectedResult = setOf(Point(0, 2))
+
+        val result : Set<Point> = findAntinodesBetween(aLocationList[0], aLocationList[1], Bounds(input))
+
+        assert(result == expectedResult)
+    }
+
+    @Test
+    fun `findAntinodesBetween should return antinodes within the bounds of the grid when the x and y differences between the points have different sign`() {
+        val input = """
+            ..........
+            ..........
+            ..........
+            ..........
+            ........a.
+            ....a.....
+            #.........
+            ..........
+            ..........
+            ..........
+        """.trimIndent().lines()
+
+        val antennaMap = parseAntennaLocations(input)
+        val aLocations = antennaMap['a']!!
+        assert(aLocations == setOf(Point(4,5), Point(8,4))) { "precondition" }
+        val aLocationList = aLocations.toList()
+
+        val expectedResult = setOf(Point(0, 6))
+
+        val result : Set<Point> = findAntinodesBetween(aLocationList[0], aLocationList[1], Bounds(input))
+
+        assert(result == expectedResult)
+    }
+
+    @Test
+    fun `findAntinodes should return all antinodes between any two members of the set`() {
+        val input = """
+            ..........
+            ...#......
+            #.........
+            ....a.....
+            ........a.
+            .....a....
+            ..#.......
+            ......A...
+            ..........
+            ..........
+        """.trimIndent().lines()
+        val bounds = Bounds(input)
+
+        val antennaMap = parseAntennaLocations(input)
+        val aLocations = antennaMap['a']!!
+        val expectedResults = bounds. filter { input[it] in setOf('A', '#') }.toSet()
+
+        val results : Set<Point> = findAntinodes(aLocations, bounds)
+
+        assert(results == expectedResults)
+    }
+
+    @Test
+    fun `part1 should return the count of unique locations that return an antinode`() {
+        assert(solution.part1(sampleInput) == 14L)
+    }
 }
