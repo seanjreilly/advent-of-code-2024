f5b2c0b1adbb7ac79ce02c8ca1b64bc331a1cc00
Sean Reilly <seanjreilly@gmail.com>
2024-12-08 07:52:42 +0000

    Day 9 Skeleton

---
+++ src/main/kotlin/day09/Day09Solution.kt
@@ -0,0 +1,12 @@
+package day09
+
+import utils.LongSolution
+
+fun main() = Day09Solution().run()
+class Day09Solution : LongSolution() {
+    override fun part1(input: List<String>): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(input: List<String>) = 0L
+}
---
+++ src/test/kotlin/day09/Day09SolutionTest.kt
@@ -0,0 +1,8 @@
+package day09
+
+class Day09SolutionTest {
+    private val sampleInput = """
+        """.trimIndent().lines()
+
+    private val solution = Day09Solution()
+}
