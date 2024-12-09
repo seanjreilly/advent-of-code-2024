e895722ad5f75f8cedd1eae2817df8bfd88b65f4
Sean Reilly <seanjreilly@gmail.com>
2024-12-09 06:41:30 +0000

    Day 9, Part 1
    Defrag using a TreeMap and a heap, which makes
    finding the last file entry and the first free
    space fairly fast.
    
    Calculate the checksum of the defragged
    filesystem — we need a long for production
    data.

--- src/main/kotlin/day09/Day09Solution.kt
+++ src/main/kotlin/day09/Day09Solution.kt
@@ -1,12 +1,73 @@
 package day09
 
 import utils.LongSolution
+import java.util.NavigableMap
+import java.util.PriorityQueue
+import java.util.TreeMap
+import kotlin.collections.component1
+import kotlin.collections.component2
+import kotlin.collections.set
 
 fun main() = Day09Solution().run()
 class Day09Solution : LongSolution() {
     override fun part1(input: List<String>): Long {
-        TODO("Not yet implemented")
+        return DiskLayout(input.first()).defrag().checksum().toLong()
     }
 
     override fun part2(input: List<String>) = 0L
 }
+
+internal fun Map<Int, File>.checksum() = this.map { (index, file) -> index * file.id }.sumOf { it.toLong() }
+
+internal data class DiskLayout(val fileLayout: NavigableMap<Int, File>, val freeSpace: List<Int>) {
+    fun defrag(): NavigableMap<Int, File> {
+        val freeSpacePq = PriorityQueue(freeSpace)
+        var freeBlock: Int? = freeSpacePq.poll()
+        while (freeBlock != null) {
+            //find the last file block on the disk
+            val (oldIndex, file) = fileLayout.pollLastEntry()
+            if (oldIndex < freeBlock) {
+                // we would be moving this file further away from the front
+                // defrag is finished, put the file back
+                fileLayout[oldIndex] = file
+                break
+            }
+            fileLayout[freeBlock] = file
+            freeBlock = freeSpacePq.poll()
+        }
+        return fileLayout
+    }
+
+    companion object {
+        internal operator fun invoke(input: String): DiskLayout {
+            var currentBlock = 0
+            val fileLayout = mutableMapOf<Int, File>()
+            val freeSpace = mutableListOf<Int>()
+
+            val digitList = input.toCharArray()
+                .map { it.digitToInt() }
+
+            digitList
+                .windowed(2, 2)
+                .withIndex()
+                .forEach { (index, list) ->
+                    val (fileSize, freeBlocks) = list
+                    val file = File(index, fileSize)
+                    repeat(fileSize) { fileLayout[currentBlock++] = file }
+                    repeat(freeBlocks) { freeSpace += (currentBlock++) }
+                }
+
+            if (digitList.size % 2 == 1) {
+                //handle the last file at the end
+                val fileSize = digitList.last()
+                val index = (digitList.size / 2)
+                val file = File(index, fileSize)
+                repeat(fileSize) { fileLayout[currentBlock++] = file }
+            }
+
+            return DiskLayout(TreeMap(fileLayout), freeSpace)
+        }
+    }
+}
+
+internal data class File(val id: Int, val blockSize: Int)
--- src/test/kotlin/day09/Day09SolutionTest.kt
+++ src/test/kotlin/day09/Day09SolutionTest.kt
@@ -1,8 +1,92 @@
 package day09
 
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import java.util.*
+
 class Day09SolutionTest {
     private val sampleInput = """
+        2333133121414131402
         """.trimIndent().lines()
 
+    private val tinySampleInput = """
+        12345
+    """.trimIndent().lines()
+
     private val solution = Day09Solution()
+
+    @Nested
+    inner class DiskLayoutTest {
+        @Test
+        fun `constructor should return the DiskLayout associated with the disk map`() {
+            val diskMap = tinySampleInput.first()
+
+            val expectedFiles = listOf(
+                File(0, 1),
+                File(1, 3),
+                File(2, 5)
+            )
+
+            val expectedFileLayout = mapOf(
+                0 to expectedFiles[0],
+                3 to expectedFiles[1],
+                4 to expectedFiles[1],
+                5 to expectedFiles[1],
+                10 to expectedFiles[2],
+                11 to expectedFiles[2],
+                12 to expectedFiles[2],
+                13 to expectedFiles[2],
+                14 to expectedFiles[2],
+            )
+
+            val expectedFreeSpace = listOf(
+                1,2,6,7,8,9
+            )
+
+            val layout = DiskLayout(diskMap)
+
+            assert(layout.fileLayout == expectedFileLayout)
+            assert(layout.freeSpace == expectedFreeSpace)
+        }
+
+        @Test
+        fun `defrag should move the ending file blocks into the earliest available free spaces`() {
+            val layout = DiskLayout(sampleInput.first())
+
+            val updatedFileLayout: NavigableMap<Int, File> = layout.defrag()
+
+            val expectedRawResult = "0099811188827773336446555566"
+            assert(updatedFileLayout.size == expectedRawResult.length)
+            expectedRawResult.forEachIndexed { index, char ->
+                val expectedFileId = char.digitToInt()
+                assert(updatedFileLayout[index]!!.id == expectedFileId)
+            }
+
+        }
+    }
+
+    @Test
+    fun `checksum should return the sum of each allocated block's position times the id of the file in the block`() {
+        val fileLayout : SortedMap<Int, File> = DiskLayout(sampleInput.first()).defrag()
+        val expectedChecksum = 1928L
+
+        val checksum: Long = fileLayout.checksum()
+
+        assert(checksum == expectedChecksum)
+    }
+
+    @Test
+    fun `part1 should parse the disk layout, defrag the disk, and return the checksum of the defragged disk`() {
+        assert(solution.part1(sampleInput) == 1928L)
+    }
+
+    @Test
+    fun `part1 should return a positive number with production input`() {
+        assert(solution.part1(solution.readInput()) > 0L)
+    }
+
+    @Test
+    fun `production input is a single line`() {
+        assert(solution.readInput().size == 1)
+    }
 }
