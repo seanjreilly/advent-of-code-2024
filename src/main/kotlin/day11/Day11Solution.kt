e23574f4847ef826a178405be72ffb1f6ea16811
Sean Reilly <seanjreilly@gmail.com>
2024-12-11 05:55:08 +0000

    Day 11, Part 1
    Build stone rules and apply 25 times
    using flatMap.

--- src/main/kotlin/day11/Day11Solution.kt
+++ src/main/kotlin/day11/Day11Solution.kt
@@ -5,8 +5,31 @@ import utils.LongSolution
 fun main() = Day11Solution().run()
 class Day11Solution : LongSolution() {
     override fun part1(input: List<String>): Long {
-        TODO("Not yet implemented")
+        var stones = input.first().toStones()
+        repeat(25) { stones = stones.blink() }
+        return stones.size.toLong()
     }
 
     override fun part2(input: List<String>) = 0L
+}
+
+internal data class Stone(val value: Long, private val stringValue: String = value.toString()) {
+    fun blink(): List<Stone> {
+        return when {
+            value == 0L -> listOf(Stone(1))
+            stringValue.isEvenLength() -> value.toString().halves().map { Stone(it.toLong()) }
+            else -> listOf(Stone(value * 2024L))
+        }
+    }
+}
+
+internal fun List<Stone>.blink() = this.flatMap { it.blink() }
+
+internal fun String.toStones() = this.split(' ').map { it.toLong() }.map {  Stone(it) }
+internal fun String.isEvenLength() = this.length % 2 == 0
+
+internal fun String.halves() : List<String> {
+    require(this.isEvenLength()) { "String must be even in length" }
+    val midPoint = this.length / 2
+    return listOf(this.slice(0 until midPoint), this.slice(midPoint until this.length))
 }
--- src/test/kotlin/day11/Day11SolutionTest.kt
+++ src/test/kotlin/day11/Day11SolutionTest.kt
@@ -1,8 +1,80 @@
 package day11
 
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
 class Day11SolutionTest {
     private val sampleInput = """
+        0 1 10 99 999
         """.trimIndent().lines()
 
     private val solution = Day11Solution()
+
+    @Test
+    fun `String dot toStones() should return a list of Stone instances`() {
+        val expectedResult = listOf(
+            Stone(0),
+            Stone(1),
+            Stone(10),
+            Stone(99),
+            Stone(999)
+        )
+
+        assert(sampleInput.first().toStones() == expectedResult)
+    }
+
+    @Test
+    fun `String dot toStones() should work for production input`() {
+        assert(solution.readInput().first().toStones().isNotEmpty())
+    }
+
+    @Test
+    fun `List of Stones dot blink() should blink every Stone in the list, preserving order`() {
+        val expectedResult = "1 2024 1 0 9 9 2021976".toStones()
+        val stones = sampleInput.first().toStones()
+
+        assert(stones.blink() == expectedResult)
+    }
+
+    @Nested
+    inner class StoneTest {
+        @Test
+        fun `blink should return a single Stone(1) given Stone(0)`() {
+            val expectedResult = listOf(Stone(1))
+
+            assert(Stone(0).blink() == expectedResult)
+        }
+
+        @Test
+        fun `blink should return two Stones with half of the digits each given a Stone with an even number of digits`() {
+            assert(Stone(12).blink() == listOf(Stone(1), Stone(2)))
+            assert(Stone(1234).blink() == listOf(Stone(12), Stone(34)))
+            assert(Stone(123456).blink() == listOf(Stone(123), Stone(456)))
+            assert(Stone(12345678).blink() == listOf(Stone(1234), Stone(5678)))
+        }
+
+        @Test
+        fun `blink should remove leading zeroes when splitting a Stone with an even number of digits into two Stones`() {
+            assert(Stone(1000).blink() == listOf(Stone(10), Stone(0)))
+        }
+
+        @Test
+        fun `blink should return a Stone with the value times 2024 given a Stone with an odd number of digits that isn't 0`() {
+            assert(Stone(1).blink() == listOf(Stone(2024)))
+        }
+    }
+    
+    @Test
+    fun `blinking six times should return the correct number of stones`() {
+        var stones = "125 17".toStones()
+        val expectedResult = 22
+        repeat(6) { stones = stones.blink() }
+
+        assert(stones.size == expectedResult)
+    }
+    
+    @Test
+    fun `part1 should blink the stones 25 times and return the number of stones`() {
+        assert(solution.part1(listOf("125 17")) == 55312L)
+    }
 }
