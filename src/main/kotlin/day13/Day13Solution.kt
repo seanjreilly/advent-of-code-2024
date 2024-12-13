19170a04f664ef66836b20dbe323e59aa54168d3
Sean Reilly <seanjreilly@gmail.com>
2024-12-13 06:33:12 +0000

    Day 13, Part 1
    Brute force the potential a presses from
    1 to 100, and for each amount of presses
    check if there's a number of b presses
    that will match.

--- src/main/kotlin/day13/Day13Solution.kt
+++ src/main/kotlin/day13/Day13Solution.kt
@@ -5,8 +5,85 @@ import utils.LongSolution
 fun main() = Day13Solution().run()
 class Day13Solution : LongSolution() {
     override fun part1(input: List<String>): Long {
-        TODO("Not yet implemented")
+        return parseClawMachines(input)
+            .map { machine -> machine.findWaysToWin().sortedBy { it.cost } }
+            .filter { it.isNotEmpty() }
+            .map { it.first() }
+            .sumOf { it.cost.toLong() }
     }
 
     override fun part2(input: List<String>) = 0L
 }
+
+internal fun parseClawMachines(input: List<String>): List<ClawMachine> {
+    var list = input
+    var result = mutableListOf<ClawMachine>()
+    while (list.isNotEmpty()) {
+        val (a,b,c) = list
+        result += ClawMachine(a,b,c)
+        list = list.dropWhile { it.isNotBlank() }.drop(1)
+    }
+    return result
+}
+
+internal data class ClawMachine(val prize: Prize, val buttonA: Button, val buttonB: Button) {
+
+    fun findWaysToWin() : Collection<WayToWin> {
+        return (0 until 100)
+            .map { aPresses ->
+                val xValue = buttonA.dX * aPresses
+                val yValue = buttonA.dY * aPresses
+
+                if (xValue > prize.x) {
+                    return@map null
+                }
+                if (yValue > prize.y) {
+                    return@map null
+                }
+
+                val remainingX = prize.x - xValue
+                val remainingY = prize.y - yValue
+
+                if (remainingX % buttonB.dX != 0) {
+                    return@map null
+                }
+
+                val bPresses = remainingX / buttonB.dX
+
+                //validate
+                if (buttonB.dY * bPresses != remainingY) {
+                    return@map null
+                }
+                WayToWin(aPresses, bPresses)
+            }
+            .filterNotNull()
+    }
+
+    companion object {
+        val BUTTON_REGEX = """Button [AB]: X\+(\d+), Y\+(\d+)""".toRegex()
+        val PRIZE_REGEX = """Prize: X=(\d+), Y=(\d+)""".toRegex()
+
+        operator fun invoke(line1: String, line2: String, line3: String): ClawMachine {
+            val buttonA = parseButton(line1)
+            val buttonB = parseButton(line2)
+            val prize = parsePrize(line3)
+            return ClawMachine(prize, buttonA, buttonB)
+        }
+
+        fun parseButton(line: String) : Button {
+            val (dX,dY) = BUTTON_REGEX.matchEntire(line)!!.destructured.toList().map { it.toInt() }
+            return Button(dX ,dY)
+        }
+        fun parsePrize(line: String) : Prize {
+            val (x,y) = PRIZE_REGEX.matchEntire(line)!!.destructured.toList().map { it.toInt() }
+            return Prize(x.toInt(), y.toInt())
+        }
+    }
+}
+
+internal data class WayToWin(val buttonAPresses: Int, val buttonBPresses: Int) {
+    val cost = (3 * buttonAPresses) + buttonBPresses
+}
+
+internal data class Prize(val x: Int, val y: Int)
+internal data class Button(val dX: Int, val dY: Int)
--- src/test/kotlin/day13/Day13SolutionTest.kt
+++ src/test/kotlin/day13/Day13SolutionTest.kt
@@ -1,8 +1,98 @@
 package day13
 
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
 class Day13SolutionTest {
     private val sampleInput = """
+        Button A: X+94, Y+34
+        Button B: X+22, Y+67
+        Prize: X=8400, Y=5400
+
+        Button A: X+26, Y+66
+        Button B: X+67, Y+21
+        Prize: X=12748, Y=12176
+
+        Button A: X+17, Y+86
+        Button B: X+84, Y+37
+        Prize: X=7870, Y=6450
+
+        Button A: X+69, Y+23
+        Button B: X+27, Y+71
+        Prize: X=18641, Y=10279
         """.trimIndent().lines()
 
     private val solution = Day13Solution()
+
+    @Test
+    fun `parseClawMachines should return a list of ClawMachines`() {
+        val first = ClawMachine(Prize(8400, 5400), Button(94, 34), Button(22, 67))
+        val second = ClawMachine(Prize(12748, 12176), Button(26, 66), Button(67, 21))
+        val third = ClawMachine(Prize(7870, 6450), Button(17, 86), Button(84, 37))
+        val fourth = ClawMachine(Prize(18641, 10279), Button(69, 23), Button(27, 71))
+
+        val result : List<ClawMachine> = parseClawMachines(sampleInput)
+
+        assert(result.size == 4)
+        assert(result[0] == first)
+        assert(result[1] == second)
+        assert(result[2] == third)
+        assert(result[3] == fourth)
+    }
+
+    @Nested
+    inner class ClawMachineTest {
+        @Test
+        fun `constructor should return a ClawMachine given three lines of input`() {
+            val (a, b, c) = sampleInput.take(3)
+            val result = ClawMachine(a,b,c)
+
+            assert(result.prize == Prize(8400, 5400))
+            assert(result.buttonA == Button(94, 34))
+            assert(result.buttonB == Button(22, 67))
+        }
+
+        @Test
+        fun `findWaysToWin should return combinations of A presses and B presses that will win the prize`() {
+            val machine: ClawMachine = parseClawMachines(sampleInput).first()
+            val result : Collection<WayToWin> = machine.findWaysToWin()
+
+            val expectedBestWayToWin = WayToWin(80, 40)
+
+            assert(expectedBestWayToWin in result)
+        }
+
+        @Test
+        fun `findWaysToWin should return combinations of A presses and B presses that will win the prize given a different claw machine`() {
+            val machine: ClawMachine = parseClawMachines(sampleInput)[2]
+            val result : Collection<WayToWin> = machine.findWaysToWin()
+
+            val expectedBestWayToWin = WayToWin(38, 86)
+
+            assert(expectedBestWayToWin in result)
+        }
+
+        @Test
+        fun `findWaysToWin should return an empty collection if no combination of A presses and B presses will win the prize`() {
+            val clawMachines = parseClawMachines(sampleInput)
+
+            assert(clawMachines[1].findWaysToWin().isEmpty())
+            assert(clawMachines[3].findWaysToWin().isEmpty())
+        }
+    }
+
+    @Test
+    fun `part1 should return the fewest number of tokens needed to win all possible prizes`() {
+        assert(solution.part1(sampleInput) == 480L)
+    }
+
+    @Nested
+    inner class WayToWinTest {
+        @Test
+        fun `cost should return 3 times aPresses plus bPresses`() {
+            val wayToWin = WayToWin(80, 40)
+
+            assert(wayToWin.cost == 280)
+        }
+    }
 }
