51d27cb4723f1f27cc51d81955a1800a3dbe894c
Sean Reilly <seanjreilly@gmail.com>
2024-12-15 07:05:03 +0000

    Day 15, Part 1
    Parse the warehouse layout, box
    locations and robot locations.
    
    Move the robot (pushing boxes as it
    goes) unless it or a box will hit a
    wall. Process all the moves and then
    return the total score.

--- src/main/kotlin/day15/Day15Solution.kt
+++ src/main/kotlin/day15/Day15Solution.kt
@@ -1,12 +1,91 @@
 package day15
 
+import utils.CardinalDirection
+import utils.CardinalDirection.East
+import utils.CardinalDirection.North
+import utils.CardinalDirection.South
+import utils.CardinalDirection.West
 import utils.LongSolution
+import utils.Point
 
 fun main() = Day15Solution().run()
 class Day15Solution : LongSolution() {
     override fun part1(input: List<String>): Long {
-        TODO("Not yet implemented")
+        val originalWarehouse = Warehouse(input)
+        return parseMoves(input)
+            .fold(originalWarehouse) { warehouse, direction -> warehouse.moveRobot(direction) }
+            .totalScore().toLong()
     }
 
     override fun part2(input: List<String>) = 0L
 }
+
+internal fun parseMove(ch: Char): CardinalDirection {
+    return when(ch) {
+        '^' -> North
+        '>' -> East
+        'v' -> South
+        '<' -> West
+        else -> throw IllegalArgumentException("Unexpected move symbol '$ch'")
+    }
+}
+
+internal fun parseMoves(input: List<String>): List<CardinalDirection> {
+    return input
+        .dropWhile { it.isNotEmpty() } //warehouse layout
+        .drop(1) //blank line
+        .flatMap { line -> line.map { parseMove(it) } }
+}
+
+internal data class Warehouse(val robotLocation: Point, val boxLocations: Set<Point>, val wallLocations: Set<Point>) {
+    fun moveRobot(direction: CardinalDirection) : Warehouse {
+        val potentialNewRobotLocation = robotLocation.move(direction)
+        var newBoxLocations = boxLocations
+        if (potentialNewRobotLocation in wallLocations) {
+            return this // can't move
+        }
+        if (potentialNewRobotLocation in boxLocations) {
+            val oldBoxLocation = potentialNewRobotLocation
+            var newBoxLocation = potentialNewRobotLocation
+            while (newBoxLocation in boxLocations) {
+                newBoxLocation = newBoxLocation.move(direction)
+            }
+            if (newBoxLocation in wallLocations) {
+                return this // can't move
+            }
+            newBoxLocations -= oldBoxLocation
+            newBoxLocations += newBoxLocation
+        }
+
+        return this.copy(robotLocation = potentialNewRobotLocation, boxLocations = newBoxLocations)
+    }
+
+    fun totalScore(): Int {
+        return boxLocations.sumOf { it.score() }
+    }
+
+    companion object {
+        internal operator fun invoke(input:List<String>) : Warehouse {
+            val rawWarehouseLayout = input.takeWhile { it.isNotEmpty() }
+
+            var robotLocation : Point? = null
+            val boxLocations = mutableSetOf<Point>()
+            val wallLocations = mutableSetOf<Point>()
+
+            rawWarehouseLayout.forEachIndexed { y, line ->
+                line.forEachIndexed { x, char ->
+                    val point = Point(x, y)
+                    when (char) {
+                        '@' -> robotLocation = point
+                        'O' -> boxLocations += point
+                        '#' -> wallLocations += point
+                    }
+                }
+            }
+
+            return Warehouse(robotLocation!!, boxLocations, wallLocations)
+        }
+    }
+}
+
+internal fun Point.score() = (100 * y) + x
--- src/test/kotlin/day15/Day15SolutionTest.kt
+++ src/test/kotlin/day15/Day15SolutionTest.kt
@@ -1,8 +1,336 @@
 package day15
 
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import utils.CardinalDirection
+import utils.CardinalDirection.*
+import utils.Point
+
 class Day15SolutionTest {
-    private val sampleInput = """
+    private val largeSampleInput = """
+        ##########
+        #..O..O.O#
+        #......O.#
+        #.OO..O.O#
+        #..O@..O.#
+        #O#..O...#
+        #O..O..O.#
+        #.OO.O.OO#
+        #....O...#
+        ##########
+
+        <vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+        vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+        ><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+        <<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+        ^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+        ^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+        >^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+        <><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+        ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+        v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
+    """.trimIndent().lines()
+
+    private val smallSampleInput = """
+        ########
+        #..O.O.#
+        ##@.O..#
+        #...O..#
+        #.#.O..#
+        #...O..#
+        #......#
+        ########
+
+        <^^>>>vv<v>>v<<
+    """.trimIndent().lines()
+
+    private val largeWarehouseFinalPosition = """
+        ##########
+        #.O.O.OOO#
+        #........#
+        #OO......#
+        #OO@.....#
+        #O#.....O#
+        #O.....OO#
+        #O.....OO#
+        #OO....OO#
+        ##########
+        
     """.trimIndent().lines()
 
     private val solution = Day15Solution()
+
+    // region parseMove tests
+
+    @Test
+    fun `parseMove should return North given ^`() {
+        val direction: CardinalDirection = parseMove('^')
+        assert(direction == North)
+    }
+
+    @Test
+    fun `parseMove should return East given greater than symbol`() {
+        val direction: CardinalDirection = parseMove('>')
+        assert(direction == East)
+    }
+
+    @Test
+    fun `parseMove should return South given v`() {
+        val direction: CardinalDirection = parseMove('v')
+        assert(direction == South)
+    }
+
+    @Test
+    fun `parseMove should return West given less than symbol`() {
+        val direction: CardinalDirection = parseMove('<')
+        assert(direction == West)
+    }
+
+    @Test
+    fun `parseMove should throw an IllegalArgumentException given any other character`() {
+        val exception = assertThrows<IllegalArgumentException> { parseMove(('x')) }
+        assert(exception.message != null)
+        assert(exception.message!!.isNotEmpty())
+    }
+
+    // endregion
+
+    @Test
+    fun `parseMoves should return the list of CardinalDirections given smallInput`() {
+        val expectedMoves = listOf(
+            West,
+            North,
+            North,
+            East,
+            East,
+            East,
+            South,
+            South,
+            West,
+            South,
+            East,
+            East,
+            South,
+            West,
+            West
+        )
+
+        val moves: List<CardinalDirection> = parseMoves(smallSampleInput)
+
+        assert(moves.size == expectedMoves.size)
+        expectedMoves.forEachIndexed { index, direction ->
+            assert(moves[index] == direction)
+        }
+    }
+
+    @Test
+    fun `parseMoves should return the expected list of moves given move input on multiple lines`() {
+        val moves: List<CardinalDirection> = parseMoves(largeSampleInput)
+        val firstThreeMoves = moves.take(3)
+        val lastThreeMoves = moves.takeLast(3)
+
+        assert(moves.size == 700)
+        assert(firstThreeMoves == listOf(West, South, South))
+        assert(lastThreeMoves == listOf(West, West, North))
+    }
+
+    @Nested
+    inner class WarehouseTest {
+        @Test
+        fun `constructor should put the robot in the expected location given small sample input`() {
+            val expectedRobotLocation = Point(2,2)
+            val warehouse = Warehouse(smallSampleInput)
+
+            val robotLocation = warehouse.robotLocation
+            assert(robotLocation == expectedRobotLocation)
+        }
+
+        @Test
+        fun `constructor should put the boxes in the expected locations given small sample input`() {
+            val expectedBoxLocations = setOf(
+                Point(3,1),
+                Point(5,1),
+                Point(4,2),
+                Point(4,3),
+                Point(4,4),
+                Point(4,5),
+            )
+            val warehouse = Warehouse(smallSampleInput)
+
+            val boxLocations:Set<Point> = warehouse.boxLocations
+            assert(boxLocations == expectedBoxLocations)
+        }
+
+        @Test
+        fun `constructor should build the top and bottom line of walls given small sample input`() {
+            val warehouse = Warehouse(smallSampleInput)
+
+            val wallLocations:Set<Point> = warehouse.wallLocations
+
+            assert(wallLocations.size == 30)
+            (0 until 8).forEach { x ->
+                assert(Point(x, 0) in wallLocations) { "north wall should be present" }
+                assert(Point(x, 7) in wallLocations) { "south wall should be present" }
+            }
+
+            (0 until 7).forEach { y ->
+                assert(Point(0, y) in wallLocations) { "west wall should be present" }
+                assert(Point(7, y) in wallLocations) { "east wall should be present" }
+            }
+
+            assert(Point(1, 2) in wallLocations) {" interior wall 1 "}
+            assert(Point(2, 4) in wallLocations) {" interior wall 2 "}
+        }
+
+        @Test
+        fun `constructor should put the robot in the expected location given large sample input`() {
+            val expectedRobotLocation = Point(4,4)
+            val warehouse = Warehouse(largeSampleInput)
+
+            val robotLocation = warehouse.robotLocation
+            assert(robotLocation == expectedRobotLocation)
+        }
+
+        // region move tests
+
+        @Test
+        fun `moveRobot should do nothing if there is a wall in this direction`() {
+            val originalWarehouse = Warehouse(smallSampleInput)
+
+            //there is a wall west of the robot
+            val updatedWarehouse = Warehouse(smallSampleInput).moveRobot(West)
+
+            assert(updatedWarehouse == originalWarehouse)
+            assert(updatedWarehouse !== originalWarehouse)
+        }
+
+        @Test
+        fun `moveRobot should move the robot if there's an empty space in that direction`() {
+            val originalWarehouse = Warehouse(smallSampleInput)
+
+
+            //there is an empty space south of the robot
+            val updatedWarehouse = originalWarehouse.moveRobot(South)
+
+            assert(updatedWarehouse != originalWarehouse)
+            assert(updatedWarehouse.robotLocation == originalWarehouse.robotLocation.south())
+            assert(originalWarehouse.boxLocations == updatedWarehouse.boxLocations)
+            assert(originalWarehouse.wallLocations == updatedWarehouse.wallLocations)
+        }
+
+        @Test
+        fun `moveRobot should move a robot and a box if the robot is next to a box and there's an empty space beyond the box`() {
+            val originalWarehouse = Warehouse(smallSampleInput).moveRobot(East)
+
+            val oldBoxLocation = Point(4, 2)
+            val newBoxLocation = Point(5, 2)
+
+            //there is a box east of the robot, and an empty space beyond that
+            val updatedWarehouse = originalWarehouse.moveRobot(East)
+
+            assert(updatedWarehouse != originalWarehouse)
+            assert(updatedWarehouse.robotLocation == originalWarehouse.robotLocation.east())
+            assert(updatedWarehouse.boxLocations.size == originalWarehouse.boxLocations.size)
+            assert(oldBoxLocation !in updatedWarehouse.boxLocations)
+            assert(newBoxLocation in updatedWarehouse.boxLocations)
+            assert(originalWarehouse.wallLocations == updatedWarehouse.wallLocations)
+        }
+
+        @Test
+        fun `moveRobot should not move anything if the robot is next to a box and there's a wall beyond the box`() {
+            val originalWarehouse = Warehouse(smallSampleInput).moveRobot(East)
+
+            //there is a box north of the robot, and a wall beyond that
+            val updatedWarehouse = originalWarehouse.copy().moveRobot(North)
+
+            assert(updatedWarehouse == originalWarehouse)
+            assert(updatedWarehouse !== originalWarehouse)
+        }
+
+        @Test
+        fun `moveRobot should move the robot and all of the boxes if the robot is next to a row of boxes and there's an empty space beyond the boxes`() {
+            val originalWarehouse = Warehouse(smallSampleInput).copy(robotLocation = Point(4, 1))
+
+            //there is a row of 4 boxes south of the robot, and an empty space beyond that
+            val updatedWarehouse = originalWarehouse.copy().moveRobot(South)
+
+            assert(updatedWarehouse !== originalWarehouse)
+            assert(updatedWarehouse.robotLocation == Point(4,2))
+            assert(updatedWarehouse.robotLocation !in updatedWarehouse.boxLocations)
+            assert(Point(4, 3) in updatedWarehouse.boxLocations)
+            assert(Point(4, 4) in updatedWarehouse.boxLocations)
+            assert(Point(4, 5) in updatedWarehouse.boxLocations)
+            assert(Point(4, 6) in updatedWarehouse.boxLocations)
+            assert(originalWarehouse.wallLocations == updatedWarehouse.wallLocations)
+        }
+
+        @Test
+        fun `moveRobot should not move anything if the robot is next to a row of boxes and there's a wall beyond the boxes`() {
+            val originalWarehouse = Warehouse(smallSampleInput).copy(robotLocation = Point(4, 1)).moveRobot(South)
+
+            //there is a row of 4 boxes south of the robot, and a wall beyond that
+            val updatedWarehouse = originalWarehouse.copy().moveRobot(South)
+
+            assert(updatedWarehouse !== originalWarehouse)
+            assert(updatedWarehouse == originalWarehouse)
+        }
+
+        // endregion
+
+        @Test
+        fun `totalScore should return the sum of all boxes scores`() {
+            val warehouse = Warehouse(largeWarehouseFinalPosition)
+
+            val score : Int = warehouse.totalScore()
+
+            assert(score == 10092)
+        }
+
+        @Test
+        fun `processing all moves should yield the expected warehouse layout given the large sample input`() {
+            val expectedFinalWarehouse = Warehouse(largeWarehouseFinalPosition)
+            val originalWarehouse = Warehouse(largeSampleInput)
+            val moves = parseMoves(largeSampleInput)
+
+            val finalWarehouse = moves.fold(originalWarehouse) { warehouse, direction -> warehouse.moveRobot(direction) }
+
+            assert(finalWarehouse == expectedFinalWarehouse)
+        }
+
+        @Test
+        fun `processing all moves should yield the expected total score given the small sample input`() {
+            val originalWarehouse = Warehouse(smallSampleInput)
+            val moves = parseMoves(smallSampleInput)
+
+            val finalWarehouse = moves.fold(originalWarehouse) { warehouse, direction -> warehouse.moveRobot(direction) }
+
+            assert(finalWarehouse.totalScore() == 2028)
+        }
+    }
+    
+    @Nested
+    inner class PointTest {
+        @Test
+        fun `score should return 100 times y plus x`() {
+            val point = Point(4,1)
+            assert(point.score() == 104)
+        }
+
+        @Test
+        fun `score should return 100 times y plus x given a different point`() {
+            val point = Point(1,4)
+            assert(point.score() == 401)
+        }
+    }
+
+    @Test
+    fun `part1 should parse the warehouse, apply all the moves and return the total score given the small sample input`() {
+        assert(solution.part1(smallSampleInput) == 2028L)
+    }
+
+    @Test
+    fun `part1 should parse the warehouse, apply all the moves and return the total score given the large sample input`() {
+        assert(solution.part1(largeSampleInput) == 10092L)
+    }
 }
