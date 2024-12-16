ccf422900ea04c333c3fb841a0ef702f45dedc76
Sean Reilly <seanjreilly@gmail.com>
2024-12-16 06:59:23 +0000

    Day 17 Skeleton

---
+++ src/main/kotlin/day17/Day17Solution.kt
@@ -0,0 +1,12 @@
+package day17
+
+import utils.LongSolution
+
+fun main() = Day17Solution().run()
+class Day17Solution : LongSolution() {
+    override fun part1(input: List<String>): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(input: List<String>) = 0L
+}
---
+++ src/test/kotlin/day17/Day17SolutionTest.kt
@@ -0,0 +1,8 @@
+package day17
+
+class Day17SolutionTest {
+    private val sampleInput = """
+    """.trimIndent().lines()
+
+    private val solution = Day17Solution()
+}
