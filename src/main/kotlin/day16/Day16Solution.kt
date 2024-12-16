c513d674f161329eb8764f2722f31fa3b326c0a7
Sean Reilly <seanjreilly@gmail.com>
2024-12-16 05:36:33 +0000

    Day 16, Part 1
    Use Dijkstra's algorithm to find the lowest cost
    route through the maze.

--- src/main/kotlin/day16/Day16Solution.kt
+++ src/main/kotlin/day16/Day16Solution.kt
@@ -1,12 +1,57 @@
 package day16
 
+import utils.Bounds
+import utils.CardinalDirection.East
 import utils.LongSolution
+import utils.Point
+import utils.PointAndDirection
+import utils.TurnDirection.*
+import utils.dijkstras
+import utils.get
 
 fun main() = Day16Solution().run()
 class Day16Solution : LongSolution() {
-    override fun part1(input: List<String>): Long {
-        TODO("Not yet implemented")
+    override fun part1(input: List<String>) = parseMaze(input).findLowestCostToEnd().toLong()
+    override fun part2(input: List<String>) = 0L
+}
+
+internal data class Maze(val start: PointAndDirection, val end: Point, val walls: Set<Point>, val bounds: Bounds) {
+    fun findLowestCostToEnd(): Int {
+
+        fun findNeighbours(current: PointAndDirection): Collection<Pair<PointAndDirection, Int>> {
+            val result = mutableListOf(
+                current.copy(direction = current.direction.turn(Left)) to 1000,
+                current.copy(direction = current.direction.turn(Right)) to 1000,
+            )
+
+            val forward = current.point.move(current.direction)
+            if (forward in bounds && forward !in walls) {
+                result += ((forward facing current.direction) to 1)
+            }
+
+            return result
+        }
+
+        return dijkstras(start, neighboursMapping = { findNeighbours(it) })
+            .filter { entry -> entry.key.point == end }
+            .values
+            .min()
+    }
+}
+
+internal fun parseMaze(input: List<String>): Maze {
+    val bounds = Bounds(input)
+    var start: PointAndDirection? = null
+    var end: Point? = null
+    val walls = mutableSetOf<Point>()
+
+    bounds.forEach {
+        when(input[it]) {
+            'S' -> start = it facing East
+            'E' -> end = it
+            '#' -> walls += it
+        }
     }
 
-    override fun part2(input: List<String>) = 0L
+    return Maze(start!!, end!!, walls, bounds)
 }
--- src/test/kotlin/day16/Day16SolutionTest.kt
+++ src/test/kotlin/day16/Day16SolutionTest.kt
@@ -1,8 +1,96 @@
 package day16
 
+import org.junit.jupiter.api.Test
+import utils.Bounds
+import utils.CardinalDirection.East
+import utils.Point
+
 class Day16SolutionTest {
     private val sampleInput = """
+        ###############
+        #.......#....E#
+        #.#.###.#.###.#
+        #.....#.#...#.#
+        #.###.#####.#.#
+        #.#.#.......#.#
+        #.#.#####.###.#
+        #...........#.#
+        ###.#.#####.#.#
+        #...#.....#.#.#
+        #.#.#.###.#.#.#
+        #.....#...#.#.#
+        #.###.#.#.#.#.#
+        #S..#.....#...#
+        ###############
+    """.trimIndent().lines()
+
+    private val secondSampleInput = """
+        #################
+        #...#...#...#..E#
+        #.#.#.#.#.#.#.#.#
+        #.#.#.#...#...#.#
+        #.#.#.#.###.#.#.#
+        #...#.#.#.....#.#
+        #.#.#.#.#.#####.#
+        #.#...#.#.#.....#
+        #.#.#####.#.###.#
+        #.#.#.......#...#
+        #.#.###.#####.###
+        #.#.#...#.....#.#
+        #.#.#.#####.###.#
+        #.#.#.........#.#
+        #.#.#.#########.#
+        #S#.............#
+        #################
     """.trimIndent().lines()
 
     private val solution = Day16Solution()
+
+    @Test
+    fun `parseMaze should return a maze with a start, end, and walls`() {
+        val expectedStart = Point(1, 13) facing East
+        val expectedEnd = Point(13, 1)
+        val expectedBounds = Bounds(0 until 15, 0 until 15)
+
+        val maze: Maze = parseMaze(sampleInput)
+
+        assert(maze.start == expectedStart)
+        assert(maze.end == expectedEnd)
+        assert(maze.bounds == expectedBounds)
+        assert(Point(0,0) in maze.walls)
+        assert(Point(1,0) in maze.walls)
+        assert(Point(0,1) in maze.walls)
+        assert(Point(1,1) !in maze.walls)
+
+        assert(maze.start.point !in maze.walls)
+        assert(maze.start.point.north() !in maze.walls)
+        assert(maze.start.point.east() !in maze.walls)
+        assert(maze.walls.size == 121)
+    }
+
+    @Test
+    fun `findLowestCostToEnd should find the lowest cost route from start to end`() {
+        val maze: Maze = parseMaze(sampleInput)
+        val expectedLowestCost = 7036
+
+        val cost: Int = maze.findLowestCostToEnd()
+
+        assert(cost == expectedLowestCost)
+    }
+
+    @Test
+    fun `findLowestCostToEnd should find the lowest cost route from start to end given the second maze`() {
+        val maze: Maze = parseMaze(secondSampleInput)
+        val expectedLowestCost = 11048
+
+        val cost: Int = maze.findLowestCostToEnd()
+
+        assert(cost == expectedLowestCost)
+    }
+
+    @Test
+    fun `part1 should return the minimum cost through the maze`() {
+        assert(solution.part1(sampleInput) == 7036L)
+    }
 }
+
