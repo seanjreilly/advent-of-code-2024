072e10d47b1f401529c0b437145a31ecce4ae6e5
Sean Reilly <seanjreilly@gmail.com>
2024-12-18 05:47:20 +0000

    Day 18, Part 1
    Use Dijkstra's algorithm, and worry about what's coming
    next. ;-)

--- src/main/kotlin/day18/Day18Solution.kt
+++ src/main/kotlin/day18/Day18Solution.kt
@@ -1,12 +1,46 @@
 package day18
 
+import utils.Bounds
+import utils.DijkstrasAlgorithm
 import utils.LongSolution
+import utils.Point
 
-fun main() = Day18Solution().run()
-class Day18Solution : LongSolution() {
+internal val PROD_BOUNDS = Bounds(0 ..70, 0..70)
+internal const val PROD_PART1_FALLEN_BLOCKS = 1024
+
+fun main() = Day18Solution(PROD_BOUNDS, PROD_PART1_FALLEN_BLOCKS).run()
+class Day18Solution(val bounds: Bounds, val part1FallenBlocks: Int) : LongSolution() {
     override fun part1(input: List<String>): Long {
-        TODO("Not yet implemented")
+        return findShortestPathToExit(bounds, parseFallingBytes(input), part1FallenBlocks).toLong()
     }
 
     override fun part2(input: List<String>) = 0L
+}
+
+internal fun findShortestPathToExit(bounds: Bounds, fallingBytes: List<Point>, bytesFallenSoFar: Int): Int {
+    val corruptedLocations:Set<Point> = fallingBytes.take(bytesFallenSoFar).toSet()
+    val endPoint = getExitPoint(bounds)
+
+    val dijkstras = DijkstrasAlgorithm<Point> { point ->
+        point.getCardinalNeighbours()
+            .filter { it in bounds  }
+            .filter { it !in corruptedLocations  }
+            .map { it to 1 }
+    }
+
+    val result = dijkstras.search(Point(0,0))
+    return result.costs[endPoint]!!
+}
+
+internal fun parseFallingBytes(input: List<String>): List<Point> {
+    return input.map(::parseFallingByte)
+}
+
+internal fun parseFallingByte(input: String): Point {
+    val (x,y) = input.split(',').map(String::toInt)
+    return Point(x,y)
+}
+
+internal fun getExitPoint(bounds: Bounds): Point {
+    return Point(bounds.lastX, bounds.lastY)
 }
--- src/test/kotlin/day18/Day18SolutionTest.kt
+++ src/test/kotlin/day18/Day18SolutionTest.kt
@@ -1,8 +1,92 @@
 package day18
 
+import org.junit.jupiter.api.Test
+import utils.Bounds
+import utils.DijkstrasAlgorithm
+import utils.Point
+
 class Day18SolutionTest {
     private val sampleInput = """
+        5,4
+        4,2
+        4,5
+        3,0
+        2,1
+        6,3
+        2,4
+        1,5
+        0,6
+        3,3
+        2,6
+        5,1
+        1,2
+        5,5
+        2,5
+        6,5
+        1,4
+        0,4
+        6,4
+        1,1
+        6,1
+        1,0
+        0,5
+        1,6
+        2,0
     """.trimIndent().lines()
 
-    private val solution = Day18Solution()
+    private val TEST_BOUNDS = Bounds(0..6, 0..6)
+    private val TEST_PART1_FALLEN_BYTES = 12
+
+    private val solution = Day18Solution(TEST_BOUNDS, TEST_PART1_FALLEN_BYTES)
+
+    @Test
+    fun `parseFallingByte should return the correct point given the first line of sample input`() {
+        val line = sampleInput.first()
+        val fallingByte : Point = parseFallingByte(line)
+        assert(fallingByte == Point(5,4))
+    }
+
+    @Test
+    fun `parseFallingByte should return the correct point given the second line of sample input`() {
+        val line = sampleInput[1]
+        val fallingByte : Point = parseFallingByte(line)
+        assert(fallingByte == Point(4,2))
+    }
+
+    @Test
+    fun `parseFallingBytes should return a list of Points`() {
+        val fallingBytes : List<Point> = parseFallingBytes(sampleInput)
+
+        assert(fallingBytes.size == 25)
+        assert(fallingBytes[0] == Point(5,4))
+        assert(fallingBytes[1] == Point(4,2))
+        assert(fallingBytes[2] == Point(4,5))
+        assert(fallingBytes[3] == Point(3,0))
+        assert(fallingBytes[4] == Point(2,1))
+        assert(fallingBytes.last() == Point(2,0))
+    }
+
+    @Test
+    fun `getExitPoint should return the bottom right corner of the bounds given test bounds`() {
+        val exitPoint: Point = getExitPoint(TEST_BOUNDS)
+        assert(exitPoint == Point(6, 6))
+    }
+
+    @Test
+    fun `getExitPoint should return the bottom right corner of the bounds given prod bounds`() {
+        val exitPoint: Point = getExitPoint(PROD_BOUNDS)
+        assert(exitPoint == Point(70, 70))
+    }
+
+    @Test
+    fun `findShortestPathToExit should use Dijkstra's to find the shortest path to the exit given a bounds, a list of falling bytes, and how many bytes have fallen, and should return the length of the shortest path`() {
+        val result: Int = findShortestPathToExit(TEST_BOUNDS, parseFallingBytes(sampleInput), 12)
+        assert(result == 22)
+    }
+
+    @Test
+    fun `part1 should return the number of nodes in the shortest path to the exit given the conditions`() {
+        assert(solution.part1(sampleInput) == 22L)
+    }
+
 }
