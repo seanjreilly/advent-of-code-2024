5f13364c30ee7c32d11d6b6c4218b875a46a164c
Sean Reilly <seanjreilly@gmail.com>
2024-12-19 05:35:47 +0000

    Day 19, part 1
    Build a recursive search using startsWith to find
    possible designs.

--- src/main/kotlin/day19/Day19Solution.kt
+++ src/main/kotlin/day19/Day19Solution.kt
@@ -5,8 +5,32 @@ import utils.LongSolution
 fun main() = Day19Solution().run()
 class Day19Solution : LongSolution() {
     override fun part1(input: List<String>): Long {
-        TODO("Not yet implemented")
+        val towelPatterns = parseTowelPatterns(input)
+        val desiredDesigns = parseDesiredDesigns(input)
+        return desiredDesigns
+            .count { isDesignPossible(it, towelPatterns) }
+            .toLong()
     }
 
     override fun part2(input: List<String>) = 0L
+}
+
+internal fun parseTowelPatterns(input: List<String>): Set<String> {
+    return input.first().split(", ").toSet()
+}
+
+internal fun parseDesiredDesigns(input: List<String>): List<String> {
+    return input.drop(2)
+}
+
+internal fun isDesignPossible(towelDesign: String, towels: Set<String>): Boolean {
+    if (towelDesign == "") {
+        return true //base case
+    }
+    return towels
+        .filter { towelDesign.startsWith(it) }
+        .map { towelDesign.substring(it.length) }
+        .any { remainingDesign ->
+            isDesignPossible(remainingDesign, towels)
+        }
 }
--- src/test/kotlin/day19/Day19SolutionTest.kt
+++ src/test/kotlin/day19/Day19SolutionTest.kt
@@ -1,8 +1,80 @@
 package day19
 
+import org.junit.jupiter.api.Test
+
 class Day19SolutionTest {
     private val sampleInput = """
+        r, wr, b, g, bwu, rb, gb, br
+
+        brwrr
+        bggr
+        gbbr
+        rrbgbr
+        ubwu
+        bwurrg
+        brgr
+        bbrgwb
     """.trimIndent().lines()
 
     private val solution = Day19Solution()
+    
+    @Test
+    fun `parseTowelPatterns should return the set of towel patterns`() {
+        val expectedTowelPatterns = setOf(
+            "r",
+            "wr",
+            "b",
+            "g",
+            "bwu",
+            "rb",
+            "gb",
+            "br"
+        )
+
+        val towelPatterns: Set<String> = parseTowelPatterns(sampleInput)
+
+        assert(towelPatterns == expectedTowelPatterns)
+    }
+
+    @Test
+    fun `parseDesiredDesigns should return the list of desired towel designs`() {
+        val expectedDesigns = listOf(
+            "brwrr",
+            "bggr",
+            "gbbr",
+            "rrbgbr",
+            "ubwu",
+            "bwurrg",
+            "brgr",
+            "bbrgwb"
+        )
+
+        val desiredDesigns: List<String> = parseDesiredDesigns(sampleInput)
+
+        assert(desiredDesigns == expectedDesigns)
+    }
+
+    @Test
+    fun `isDesignPossible should return true if the design can be made with a combination of available towels`() {
+        val towelPatterns = parseTowelPatterns(sampleInput)
+        assert(isDesignPossible("brwrr", towelPatterns))
+        assert(isDesignPossible("bggr", towelPatterns))
+        assert(isDesignPossible("gbbr", towelPatterns))
+        assert(isDesignPossible("rrbgbr", towelPatterns))
+        assert(isDesignPossible("bwurrg", towelPatterns))
+        assert(isDesignPossible("brgr", towelPatterns))
+    }
+
+    @Test
+    fun `isDesignPossible should return false if the design cannot be made with a combination of available towels`() {
+        val towelPatterns = parseTowelPatterns(sampleInput)
+        assert(!isDesignPossible("ubwu", towelPatterns))
+        assert(!isDesignPossible("bbrgwb", towelPatterns))
+
+    }
+
+    @Test
+    fun `part1 should count the number of designs that are possible`() {
+        assert(solution.part1(sampleInput) == 6L)
+    }
 }
