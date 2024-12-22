d8fe673b36d860b1a48991b11c8a718ee5cb8220
Sean Reilly <seanjreilly@gmail.com>
2024-12-22 10:30:44 +0000

    Day 22, Part 2
    Generate 2000 pseudorandom numbers for each line of input,
    and calculate the prices (mod 10) and price changes for each
    sequence. Find which sequence of changes will yield bananas
    for each secret number sequence, removing duplicates.
    
    Merge the results for each secret number, and then find the
    price change pattern that yields the largest result and return
    that result.
    
    This generates a result for part 2 in ~3.5 seconds.

--- src/main/kotlin/day22/Day22Solution.kt
+++ src/main/kotlin/day22/Day22Solution.kt
@@ -10,8 +10,57 @@ class Day22Solution : LongSolution() {
             .sumOf { nextSecretNumberAfterNRounds(it, 2000) }
     }
 
-    override fun part2(input: List<String>) = 0L
+    override fun part2(input: List<String>): Long {
+        return findBestPriceChange(input.map { it.toLong() }).second
+    }
 }
+
+internal fun findBestPriceChange(secretNumbers: List<Long>): Pair<PriceChangePattern, Long> {
+    return secretNumbers
+        .map { removeDuplicates(findChangesAndPrices(it, 2000)) }
+        .reduce(::merge)
+        .maxBy { it.value }
+        .toPair()
+}
+
+internal fun merge(first: Map<PriceChangePattern, Long>, second: Map<PriceChangePattern, Long>): Map<PriceChangePattern, Long> {
+    val result = first.toMutableMap()
+    second.forEach { (key, value) -> result.merge(key, value, Long::plus) }
+    return result
+}
+
+internal fun removeDuplicates(prices: List<Pair<PriceChangePattern, Long>>): Map<PriceChangePattern, Long> {
+    return prices.asReversed().associate { it }
+}
+
+internal fun findChangesAndPrices(secretNumber: Long, rounds: Int): List<Pair<PriceChangePattern, Long>> {
+    val prices = mutableListOf<Int>(secretNumber.mod(10))
+
+    var currentSecretNumber = secretNumber
+    var roundsRemaining = rounds
+    do {
+        currentSecretNumber = nextSecretNumber(currentSecretNumber)
+        prices += currentSecretNumber.mod(10)
+        roundsRemaining--
+    } while (roundsRemaining > 0)
+
+    fun convertWindow(window: List<Int>) : Pair<PriceChangePattern, Long> {
+        val window_1 = window[1]
+        val window_2 = window[2]
+        val window_3 = window[3]
+        val window_4 = window[4]
+        val firstChange = (window_1 - window[0]).toByte()
+        val secondChange = (window_2 - window_1).toByte()
+        val thirdChange = (window_3 - window_2).toByte()
+        val fourthChange = (window_4 - window_3).toByte()
+        return PriceChangePattern(firstChange, secondChange, thirdChange, fourthChange) to window_4.toLong()
+    }
+
+    return prices
+        .windowed(5)
+        .map { convertWindow(it) }
+}
+internal data class PriceChangePattern(val firstChange : Byte, val secondChange: Byte, val thirdChange: Byte, val fourthChange: Byte)
 
 internal fun nextSecretNumberAfterNRounds(secretNumber: Long, rounds: Int): Long {
     var currentValue = secretNumber
