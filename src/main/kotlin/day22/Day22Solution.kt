5d5df429a682c7878c83b637ef801040d0c575b4
Sean Reilly <seanjreilly@gmail.com>
2024-12-22 07:52:04 +0000

    Day 22, Part 1
    Generate 2000 pseudorandom numbers for each line of input,
    and calculate the sum of the 2000th numbers.

--- src/main/kotlin/day22/Day22Solution.kt
+++ src/main/kotlin/day22/Day22Solution.kt
@@ -5,8 +5,31 @@ import utils.LongSolution
 fun main() = Day22Solution().run()
 class Day22Solution : LongSolution() {
     override fun part1(input: List<String>): Long {
-        TODO("Not yet implemented")
+        return input
+            .map { it.toLong() }
+            .sumOf { nextSecretNumberAfterNRounds(it, 2000) }
     }
 
     override fun part2(input: List<String>) = 0L
 }
+
+internal fun nextSecretNumberAfterNRounds(secretNumber: Long, rounds: Int): Long {
+    var currentValue = secretNumber
+    var roundsRemaining = rounds
+    do {
+        currentValue = nextSecretNumber(currentValue)
+        roundsRemaining--
+    } while (roundsRemaining > 0)
+    return currentValue
+}
+
+internal fun nextSecretNumber(secretNumber: Long): Long {
+    var result = secretNumber
+    result = prune(mix(result shl 6, result))
+    result = prune(mix(result shr 5, result))
+    result = prune(mix(result shl 11, result))
+    return result
+}
+
+internal fun mix(numberToMix: Long, secretNumber: Long) = numberToMix xor secretNumber
+internal fun prune(secretNumber: Long) = secretNumber and 16777215L
