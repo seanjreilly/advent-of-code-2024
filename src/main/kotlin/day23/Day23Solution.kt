d31aababc0379769e41925e04e678fb195a0da1b
Sean Reilly <seanjreilly@gmail.com>
2024-12-22 10:41:18 +0000

    Day 23 Skeleton

---
+++ src/main/kotlin/day23/Day23Solution.kt
@@ -0,0 +1,12 @@
+package day23
+
+import utils.LongSolution
+
+fun main() = Day23Solution().run()
+class Day23Solution : LongSolution() {
+    override fun part1(input: List<String>): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(input: List<String>) = 0L
+}
---
+++ src/test/kotlin/day23/Day23SolutionTest.kt
@@ -0,0 +1,8 @@
+package day23
+
+class Day23SolutionTest {
+    private val sampleInput = """
+    """.trimIndent().lines()
+
+    private val solution = Day23Solution()
+}
