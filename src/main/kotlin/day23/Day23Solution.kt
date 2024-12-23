1fddcba4ba6382cc051f98381ea6b2ab3d2b1eda
Sean Reilly <seanjreilly@gmail.com>
2024-12-23 10:44:17 +0000

    Day 23, Part 1
    Build a map of bidirectional edges and then loop over the
    forward edges only: if any two nodes this node connects to
    also connect to each other, then it's a set of three.
    
    Filter the sets of three to find ones containing nodes that
    start with 't' and return the count.

--- src/main/kotlin/day23/Day23Solution.kt
+++ src/main/kotlin/day23/Day23Solution.kt
@@ -1,12 +1,45 @@
 package day23
 
 import utils.LongSolution
+import utils.twoElementCombinations
 
 fun main() = Day23Solution().run()
 class Day23Solution : LongSolution() {
     override fun part1(input: List<String>): Long {
-        TODO("Not yet implemented")
+        return containNodeStartingWithT(findSetsOfThree(input)).count().toLong()
     }
 
     override fun part2(input: List<String>) = 0L
+}
+
+internal fun containNodeStartingWithT(nodeSets: Set<Set<String>>): Collection<Set<String>> {
+    return nodeSets.filter { set -> set.any { it.startsWith('t') } }
+}
+
+internal fun findSetsOfThree(input: List<String>): Set<Set<String>> {
+    val rawEdges: List<Pair<String,String>> = input
+        .map { it.split('-') }
+        .map { (first, second) -> first to second }
+
+    //forward edges first
+    val biDirectionalEdges = rawEdges.groupBy({ it.first }, { it.second })
+        .mapValues { it.value.toSet() }
+        .toMutableMap()
+
+    //add reverse edges
+    rawEdges.groupBy({ it.second }, { it.first })
+        .mapValues { it.value.toSet() }
+        .forEach { key, values ->
+            biDirectionalEdges.merge(key, values, Set<String>::plus)
+        }
+
+    return biDirectionalEdges.keys.flatMap { startNode ->
+        biDirectionalEdges[startNode]!!.twoElementCombinations()
+            .mapNotNull { (first, second) ->
+                when (second in biDirectionalEdges[first]!!) {
+                    true -> setOf(startNode, first, second)
+                    false -> null
+                }
+            }
+    }.toSet()
 }
--- src/test/kotlin/day23/Day23SolutionTest.kt
+++ src/test/kotlin/day23/Day23SolutionTest.kt
@@ -1,8 +1,97 @@
 package day23
 
+import org.junit.jupiter.api.Test
+
 class Day23SolutionTest {
     private val sampleInput = """
+        kh-tc
+        qp-kh
+        de-cg
+        ka-co
+        yn-aq
+        qp-ub
+        cg-tb
+        vc-aq
+        tb-ka
+        wh-tc
+        yn-cg
+        kh-ub
+        ta-co
+        de-co
+        tc-td
+        tb-wq
+        wh-td
+        ta-ka
+        td-qp
+        aq-cg
+        wq-ub
+        ub-vc
+        de-ta
+        wq-aq
+        wq-vc
+        wh-yn
+        ka-de
+        kh-ta
+        co-tc
+        wh-qp
+        tb-vc
+        td-yn
+    """.trimIndent().lines()
+
+    private val sampleInterconnectedSetsOfThree = """
+        aq,cg,yn
+        aq,vc,wq
+        co,de,ka
+        co,de,ta
+        co,ka,ta
+        de,ka,ta
+        kh,qp,ub
+        qp,td,wh
+        tb,vc,wq
+        tc,td,wh
+        td,wh,yn
+        ub,vc,wq
+    """.trimIndent().lines()
+
+    private val sampleInterconnectedSetsOfThreeWithAtLeastOneNodeStartingWithT = """
+        co,de,ta
+        co,ka,ta
+        de,ka,ta
+        qp,td,wh
+        tb,vc,wq
+        tc,td,wh
+        td,wh,yn
     """.trimIndent().lines()
 
     private val solution = Day23Solution()
+
+    @Test
+    fun `findSetsOfThree should return a set of sets of three interconnected nodes given a list of bidirectional edges`() {
+        val expectedSetsOfThree = toSetOfSets(sampleInterconnectedSetsOfThree)
+
+        val result: Set<Set<String>> = findSetsOfThree(sampleInput)
+
+        assert(result == expectedSetsOfThree)
+    }
+
+    @Test
+    fun `containNodeStartingWithT should return the sets containing at least one node starting with t given sets of nodes`() {
+        val setsOfThree = toSetOfSets(sampleInterconnectedSetsOfThree)
+        val expectedSetsWithTNodes = toSetOfSets(sampleInterconnectedSetsOfThreeWithAtLeastOneNodeStartingWithT)
+
+        val result: Collection<Set<String>> = containNodeStartingWithT(setsOfThree).toSet()
+
+        assert(result == expectedSetsWithTNodes)
+    }
+
+    @Test
+    fun `part1 should return the number of connected sets of 3 nodes containing at least one node starting with t`() {
+        assert(solution.part1(sampleInput) == 7L)
+    }
+
+    private fun toSetOfSets(input: List<String>): Set<Set<String>> {
+        return input
+            .map { it.split(',').toSet() }
+            .toSet()
+    }
 }
