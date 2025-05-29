f925972af7c361b9f37d90240e3733dfe34b5af1
Sean Reilly <seanjreilly@gmail.com>
2025-05-29 11:23:35 +0100

    Upgrade to kotlin 2.1.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.0.21"
-    kotlin("plugin.power-assert") version "2.0.21"
+    kotlin("jvm") version "2.1.21"
+    kotlin("plugin.power-assert") version "2.1.21"
     `jvm-test-suite`
 }
 
